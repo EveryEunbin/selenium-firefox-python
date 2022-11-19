e4c889464f91fd181ccb76ca87def9468e864139
EveryEunbin <116586151+EveryEunbin@users.noreply.github.com>
2022-11-19 23:46:10 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
+options = FirefoxOptions()
+options.add_argument("--headless")
+driver = webdriver.Firefox(options=options)
+driver.get("https://google.com")
+print(driver.title)
